aeda6e6ae5a7fcd6dfff35ab0d8f05d3498fcc02
(Emon) Habibur Rahman <accounts@emon.no>
2026-03-23 11:56:01 +0000

    clean up of old files and folders

--- uix_dep/frontend/src/components/ProjectTabs/Modeling.jsx
+++
@@ -1,5 +0,0 @@
-function Modeling() {
-    return <p>This is the overview of the project.</p>
-  }
-  
-  export default Modeling
--- uix_dep/frontend/src/components/ProjectTabs/SimulatorConfigTabs/openEmsConfigTab.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function openEMSConfigTab() {
-  return <div>Work in progress</div>;
-}
-
-export default openEMSConfigTab;
